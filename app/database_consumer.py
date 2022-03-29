743e851a56f06ad96c50962554006c24961a2c76
Vishruth Veerendranath <vishruth2002@gmail.com>
2022-03-29 19:21:51 +0530

    Fixed Database consumer

--- app/database_consumer.py
+++ app/database_consumer.py
@@ -10,11 +10,7 @@ import sys
 import pymongo
 
 def main():
-    server_ip = os.environ.get('PRODUCER_ADDRESS')
-    # server_port = os.environ.get('server_port')
-    consumer_id = os.environ.get('CONSUMER_ID')
-
-    def ride_matching_callback_store(ch, method, properties, body):
+    def store_callback(ch, method, properties, body):
         ride_details = json.loads(body)
         print(" [x] Received %r" % body.decode())
         
@@ -22,41 +18,31 @@ def main():
         mydb = myclient["ride_details_db"] # create DB  
         rides = mydb["ride_details"] # create collection
 
-        if rides in mydb.list_collection_names(): # check if collection exists
+        if "ride_details" in mydb.list_collection_names(): # check if collection exists
             x = rides.insert_one(ride_details)
-            print(" [x] %r inserted into DB\n" % method.delivery_tag)
+            print(" [x] Record %r inserted into DB with ID: %r\n" % (method.delivery_tag, x.inserted_id))
         else:
             print('Collection not created yet..')
 
-        # ride_details = {"pickup":"", "destination": "", "time": "", "cost": "", "seats": ""}
-
 
     try:
-        if(register.status_code == 200):
-            try:
-                connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
-            except pika.exceptions.AMQPConnectionError as exc:
-                print("Failed to connect to RabbitMQ service. Message wont be sent.")
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+    except pika.exceptions.AMQPConnectionError as exc:
+        print("Failed to connect to RabbitMQ service. Message wont be sent.")
 
-            connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-            channel = connection.channel()
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
 
-            # channel.queue_declare(queue='ride_match')
+    # channel.queue_declare(queue='database')
 
-            channel.basic_consume(
-                queue='ride_match', 
-                auto_ack=True, 
-                on_message_callback=ride_matching_callback
-            )
+    channel.basic_consume(
+        queue='database', 
+        auto_ack=True, 
+        on_message_callback=store_callback
+    )
 
-            print(' [*] Waiting for messages. To exit press CTRL+C')
-            channel.start_consuming()
-        else:
-            print('Failed Request', register.status_code)
-
-    except Exception as e:
-        print('Invalid IP Address')
-
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
 
 if __name__ == '__main__':
     try:
